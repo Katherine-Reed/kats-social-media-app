73695a593c44fe9bbde44b120ff6389cfee4dee4
Kat Grace <katherine.grace.reed@gmail.com>
2024-11-10 11:40:57 +0000

    Add post boilerplate

--- data.js
+++ data.js
@@ -3,19 +3,19 @@ export const chatsData = [
         handle: `@TrollBot66756542 💎`,
         profilePic: `images/troll.png`,
         likes: 27,
-        rechats: 10,
+        shares: 10,
         chatText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
             Guaranteed return on investment. HMU DMs open!!`,
         replies: [],
         isLiked: false,
-        isRechated: false,
+        isShared: false,
         uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
     },    
     {
         handle: `@Teflon ✅`,
         profilePic: `images/mask.png`,
         likes: 6500,
-        rechats: 234,
+        shares: 234,
         chatText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
                   {
@@ -30,14 +30,14 @@ export const chatsData = [
             },
         ],
         isLiked: false,
-        isRechated: false,
+        isShared: false,
         uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
     },
         {
         handle: `@NoobCoder12`,
         profilePic: `images/overflow.png`,
         likes: 10,
-        rechats: 3,
+        shares: 3,
         chatText: `Are you a coder if you only know HTML?`,
         replies: [
             {
@@ -52,7 +52,7 @@ export const chatsData = [
             },
         ],
         isLiked: false,
-        isRechated: false,
+        isShared: false,
         uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
     },     
 ]
--- index.js
+++ index.js
@@ -6,3 +6,35 @@ const chatBtn = document.getElementById('chat-btn')
 chatBtn.addEventListener('click', function() {
     console.log(chatInput)
 })
+
+function getConversationHtml(){
+    let conversationHtml = ''
+
+    for (let chat of chatsData){
+        conversationHtml = `
+        <article class="chat">
+            <section class="chat-inner">
+                <img src="${chat.profilePic}" class="profile-pic">
+                <section>
+                    <p class="handle">${chat.handle}</p>
+                    <p class="chat-text">${chat.chatText}</p>
+                    </section>
+                    <section class="chat-details">
+                    <span class="chat-detail">
+                        ${chat.replies.length}
+                    </span>
+                    <span class="chat-detail">
+                        ${chat.likes}
+                    </span>
+                    <span class="chat-detail">
+                        ${chat.shares}
+                    </span>
+                </section>
+            </section>
+        </article>
+        `
+    }
+    return conversationHtml
+}
+
+getConversationHtml()
